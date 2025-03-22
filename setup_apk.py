5dac6239eff748e4c9759b145baa605dba37c51a
asfu222 <196063210+asfu222@users.noreply.github.com>
2025-03-22 09:03:34 -0400

    refactored apk download

--- setup_apk.py
+++ setup_apk.py
@@ -1,45 +1,7 @@
-from lib.downloader import FileDownloader
-from lib.console import ProgressBar, notice
 from utils.util import ZipUtils
 from os import path
-import os
-APK_URL = "https://d.apkpure.com/b/XAPK/com.YostarJP.BlueArchive?version=latest&nc=arm64-v8a&sv=24"
+from download_xapk import download_xapk
 TEMP_DIR = "Temp"
-os.makedirs(TEMP_DIR, exist_ok=True)
-def download_apk_file(apk_url: str) -> str:
-    if not (
-        (
-            apk_req := FileDownloader(
-                apk_url,
-                request_method="get",
-                use_cloud_scraper=True,
-            )
-        )
-        and (apk_data := apk_req.get_response(True))
-    ):
-        raise LookupError("Cannot fetch apk info.")
-
-    apk_path = path.join(
-        TEMP_DIR,
-        apk_data.headers["Content-Disposition"]
-        .rsplit('"', 2)[-2]
-        .encode("ISO8859-1")
-        .decode(),
-    )
-    apk_size = int(apk_data.headers.get("Content-Length", 0))
-
-    if path.exists(apk_path) and path.getsize(apk_path) == apk_size:
-        return apk_path
-
-    FileDownloader(
-        apk_url,
-        request_method="get",
-        enable_progress=True,
-        use_cloud_scraper=True,
-    ).save_file(apk_path)
-
-    return apk_path
-
 def extract_apk_file(apk_path: str) -> None:
     """Extract the XAPK file."""
     apk_files = ZipUtils.extract_zip(
@@ -51,6 +13,5 @@ def extract_apk_file(apk_path: str) -> None:
     )
 
 if not path.exists(path.join(TEMP_DIR, "Data")):
-    notice("Download APK and setting up resources.")
-    apk_path = download_apk_file(APK_URL)
+    apk_path = download_xapk()
     extract_apk_file(apk_path)
