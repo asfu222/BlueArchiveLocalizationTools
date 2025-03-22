989fda827e46ffa343fdaf9199c6c0884008c115
asfu222 <196063210+asfu222@users.noreply.github.com>
2025-03-22 07:55:19 -0400

    remove crcmanip

--- extern_tools/crc_manipulation_py/main.py
+++
@@ -1,3 +0,0 @@
-from crcmanip.cli import cli
-if __name__ == "__main__":
-    cli()
--- extern_tools/crc_manipulation_py/setup.py
+++
@@ -1,8 +0,0 @@
-from setuptools import setup, Extension
-
-module = Extension('crcmanip.fastcrc', sources=['./crcmanip/fastcrc.c'])
-
-setup(name='crcmanip',
-      version='1.0',
-      description='CRC manipulation library',
-      ext_modules=[module])
