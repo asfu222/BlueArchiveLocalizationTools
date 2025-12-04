4d1217b752116e69530d295458790164b303ebd2
asfu222 <196063210+asfu222@users.noreply.github.com>
2025-12-03 21:36:41 -0500

    Update bundle.py

--- xtractor/bundle.py
+++ xtractor/bundle.py
@@ -7,14 +7,6 @@ from os import path
 from typing import Any, Literal
 
 import UnityPy
-import UnityPy.enums
-import UnityPy.lib
-import UnityPy.lib.FMOD
-import UnityPy.lib.FMOD.Windows
-import UnityPy.lib.FMOD.Windows.x64
-import UnityPy.lib.FMOD.Windows.x86
-import UnityPy.tools
-import UnityPy.tools.extractor
 
 from lib.console import ProgressBar
 
